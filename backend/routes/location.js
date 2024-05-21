fda1d45fa7b61ec74c8e527b00de3548a4eefd70
dom <limoudom2001@gmail.com>
2024-05-21 11:09:26 +1000

    removed unnecessary comments

--- backend/routes/location.js
+++ backend/routes/location.js
@@ -16,45 +16,6 @@ router.get("/:id", getLocation, (req, res) => {
   res.json(res.location);
 });
 
-// // Creating one
-// router.post("/", async (req, res) => {
-//   const subscriber = new Subscriber({
-//     name: req.body.name,
-//     subscribedToChannel: req.body.subscribedToChannel,
-//   });
-//   try {
-//     const newSubscriber = await subscriber.save();
-//     res.status(201).json(newSubscriber);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // Updating One
-// router.patch("/:id", getSubscriber, async (req, res) => {
-//   if (req.body.name != null) {
-//     res.subscriber.name = req.body.name;
-//   }
-//   if (req.body.subscribedToChannel != null) {
-//     res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
-//   }
-//   try {
-//     const updatedSubscriber = await res.subscriber.save();
-//     res.json(updatedSubscriber);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // Deleting One
-// router.delete("/:id", getSubscriber, async (req, res) => {
-//   try {
-//     await res.subscriber.remove();
-//     res.json({ message: "Deleted Subscriber" });
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
 
 async function getLocation(req, res, next) {
   let location;
